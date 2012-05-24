1bb409ce40eb35d59d620b51408b027ffe27bccc
Andreas <anty@Lapanty.chello.at>
2012-05-24 15:56:14 +0200

    Add requestQueue to RequestGenerator

--- src/at/ac/tuwien/lsdc/generator/RequestGenerator.java
+++ src/at/ac/tuwien/lsdc/generator/RequestGenerator.java
@@ -1,5 +1,19 @@
 package at.ac.tuwien.lsdc.generator;
 
-public class RequestGenerator {
+import java.util.List;
 
+public class RequestGenerator {
+	private List<Request> requests;
+
+	public List<Request> getRequests() {
+		return requests;
+	}
+
+	public void setRequests(List<Request> requests) {
+		this.requests = requests;
+	}
+	
+	public void addRequest(Request request) {
+		this.requests.add(request);
+	}
 }
