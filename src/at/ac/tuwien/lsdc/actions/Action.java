07bc6e3d6c04d162a4346d24bdb3de515938421c
unknown <Gerald@.(none)>
2012-05-24 14:37:06 +0200

    d

---
+++ src/at/ac/tuwien/lsdc/actions/Action.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.lsdc.actions;
+
+public abstract class Action {
+	
+	public abstract int predict();
+	
+	public abstract int estimate();
+	
+	public abstract boolean preconditions();
+}
