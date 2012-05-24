aa0899439b2dfeb8d1c5762555d95da2ce89bdca
Andreas Klejna <anty@Lapanty.chello.at>
2012-05-24 14:43:28 +0200

    Add TODOs to Analyser

--- src/at/ac/tuwien/lsdc/mape/Analyser.java
+++ src/at/ac/tuwien/lsdc/mape/Analyser.java
@@ -2,6 +2,15 @@ package at.ac.tuwien.lsdc.mape;
 
 public class Analyser {
 	public void getTopProblem() {
+		// Check SLAs against real data (red, orange, green)
 		// TODO
+		
+		// Check if there are one or more requests in the queue
+		// TODO
+		
+		// Decide what the top problem is
+		// TODO
+		
+		// return Problem;
 	}
 }
--- src/at/ac/tuwien/lsdc/mape/Executer.java
+++ src/at/ac/tuwien/lsdc/mape/Executer.java
@@ -1,5 +1,7 @@
 package at.ac.tuwien.lsdc.mape;
 
 public class Executer {
-
+	public void execute() {
+		// TODO
+	}
 }
