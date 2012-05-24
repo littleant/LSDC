1e40ad0859b492388c30622c545987dc76796183
Andreas <anty@Lapanty.chello.at>
2012-05-24 16:02:21 +0200

    Change RequestGenerator to Singleton

--- src/at/ac/tuwien/lsdc/generator/RequestGenerator.java
+++ src/at/ac/tuwien/lsdc/generator/RequestGenerator.java
@@ -3,6 +3,20 @@ package at.ac.tuwien.lsdc.generator;
 import java.util.List;
 
 public class RequestGenerator {
+	static volatile RequestGenerator instance;
+	
+	public static RequestGenerator getInstance() {
+		if (RequestGenerator.instance == null) {
+			synchronized(RequestGenerator.class) {
+				if (RequestGenerator.instance == null) {
+					RequestGenerator.instance = new RequestGenerator();
+				}
+			}
+		}
+		
+		return RequestGenerator.instance;
+	}
+	
 	private List<Request> requests;
 
 	public List<Request> getRequests() {
--- src/at/ac/tuwien/lsdc/mape/Analyser.java
+++ src/at/ac/tuwien/lsdc/mape/Analyser.java
@@ -1,11 +1,18 @@
 package at.ac.tuwien.lsdc.mape;
 
+import java.util.List;
+
+import at.ac.tuwien.lsdc.generator.Request;
+import at.ac.tuwien.lsdc.generator.RequestGenerator;
+
 public class Analyser {
 	public Problem getTopProblem() {
 		// Check SLAs against real data (red, orange, green)
 		// TODO
 		
 		// Check if there are one or more requests in the queue
+		RequestGenerator requestGenerator = RequestGenerator.getInstance();
+		List<Request> requests = requestGenerator.getRequests();
 		// TODO
 		
 		// Decide what the top problem is
