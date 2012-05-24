6041b0e0cb4e82b13cdd044c88fb887ce57bbd44
Andreas <anty@Lapanty.chello.at>
2012-05-24 18:02:27 +0200

    Fix Request and RequestGenerator

--- src/at/ac/tuwien/lsdc/generator/RequestGenerator.java
+++ src/at/ac/tuwien/lsdc/generator/RequestGenerator.java
@@ -4,17 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class RequestGenerator {
 	static volatile RequestGenerator instance;
 	
 	int ticks;
-	List<Integer> cpu;
-	List<Integer> memory;
-	List<Integer> storage;
-	List<Integer> slas;
 
 	// RequestQueue
 	private List<Request> requests;
@@ -45,7 +41,8 @@ public class RequestGenerator {
 	
 	
 	public void generateRequest() {
-
+		Request request = new Request();
+		
 		FileReader input = null;
 		try {
 			input = new FileReader("input.csv");
@@ -65,54 +62,35 @@ public class RequestGenerator {
 		String[] values = line.split(";");
 		System.out.println("Size of the values " + values.length);
 		
-		cpu = new ArrayList<Integer>();
-		memory = new ArrayList<Integer>();
-		storage = new ArrayList<Integer>();
-		slas = new ArrayList<Integer>();
-		
-		
+		List<Integer> slas = new LinkedList<Integer>();
 		int i;
 		for (i = 1; i < 4;i++)
 		slas.add(Integer.parseInt(values[i]));
-		System.out.println("Size of the slas " + this.slas.size());
 		
+		request.setCpu(slas.get(0));
+		request.setMemory(slas.get(1));
+		request.setStorage(slas.get(2));
+		System.out.println("Size of the slas " + slas.size());
+		
+		LinkedList<Integer> cpu = new LinkedList<Integer>();
 		for (; i < Integer.parseInt(values[0])+4;i++)
 		cpu.add(Integer.parseInt(values[i]));
-		System.out.println("Size of the cpus " + this.cpu.size());
+		request.setCpuUsage(cpu);
+		System.out.println("Size of the cpus " + cpu.size());
 		
+		LinkedList<Integer> memory = new LinkedList<Integer>();
 		for (; i < 2*Integer.parseInt(values[0])+4;i++)
 		memory.add(Integer.parseInt(values[i]));
-		System.out.println("Size of the memory " + this.memory.size());
+		request.setMemoryUsage(memory);
+		System.out.println("Size of the memory " + memory.size());
 		
-		
+		LinkedList<Integer> storage = new LinkedList<Integer>();
 		for (; i < 3*Integer.parseInt(values[0])+4;i++)
 		storage.add(Integer.parseInt(values[i]));
-		System.out.println("Size of the storage " + this.storage.size());
+		request.setStorageUsage(storage);
+		System.out.println("Size of the storage " + storage.size());
 		
 
-	}
-	
-	@Override
-	public String toString(){
-		
-		String request= "SLAS: ";
-
-		for (int i =0;i <this.slas.size();i++)
-			request +=slas.get(i)+";";
-
-		request += "\nCpus: ";
-		for (int i =0;i <this.cpu.size();i++)
-			request +=cpu.get(i)+";";
-		request += "\nMemory: ";
-		for (int i =0;i <this.memory.size();i++)
-			request +=memory.get(i)+";";
-		request += "\nStorage: ";
-		for (int i =0;i <this.storage.size();i++)
-			request +=storage.get(i)+";";
-		
-		
-		return request;
-		
 	}
 
 }
