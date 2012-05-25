1190c9b95bae5e2538b770e2037c2eee1f67cb44
Andreas <anty@Lapanty.chello.at>
2012-05-25 12:21:48 +0200

    Changed SchedulingAgent to be a singleton

--- src/at/ac/tuwien/lsdc/mape/SchedulingAgent.java
+++ src/at/ac/tuwien/lsdc/mape/SchedulingAgent.java
@@ -1,5 +1,25 @@
 package at.ac.tuwien.lsdc.mape;
 
 public class SchedulingAgent {
+	private static volatile SchedulingAgent instance;
 	
+	private SchedulingAgent() {
+		// do nothing
+	}
+	
+	public static SchedulingAgent getInstance() {
+		if (SchedulingAgent.instance == null) {
+			synchronized (SchedulingAgent.class) {
+				if (SchedulingAgent.instance == null) {
+					SchedulingAgent.instance = new SchedulingAgent();
+				}
+			}
+		}
+		
+		return SchedulingAgent.instance;
+	}
+	
+	public void start() {
+		// TODO: start simulation and scheduling
+	}
 }
