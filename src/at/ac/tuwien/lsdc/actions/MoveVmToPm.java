a948a49df00a5a7c352bdfeeecfdc9f9c830d46c
Andreas <anty@Lapanty.chello.at>
2012-05-31 12:11:35 +0200

    Add MoveVmToPm action

---
+++ src/at/ac/tuwien/lsdc/actions/MoveVmToPm.java
@@ -0,0 +1,252 @@
+package at.ac.tuwien.lsdc.actions;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.math3.random.RandomData;
+import org.apache.commons.math3.random.RandomDataImpl;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instance;
+import weka.core.Instances;
+import at.ac.tuwien.lsdc.Configuration;
+import at.ac.tuwien.lsdc.mape.Monitor;
+import at.ac.tuwien.lsdc.resources.App;
+import at.ac.tuwien.lsdc.resources.PhysicalMachine;
+import at.ac.tuwien.lsdc.resources.Resource;
+import at.ac.tuwien.lsdc.resources.VirtualMachine;
+
+public class MoveVmToPm extends Action {
+	private static Instances knowledgeBase = null;
+	private static Classifier classifier = null;
+	private static Evaluation evaluation = null;
+	private static RandomData randomData = new RandomDataImpl();
+	
+	private VirtualMachine vm;
+	private boolean preconditionsOk = false;
+	private int costs = 0;
+	private PhysicalMachine selectedPm = null;
+	private static int vmStartupCosts = 10;
+	
+	private int waitForEvaluation = 0;
+
+	public static Instances getKnowledgeBase() {
+		if (knowledgeBase == null) {
+			try {
+				//load knowledgebase from file
+				MoveVmToPm.knowledgeBase = Action.loadKnowledge(Configuration.getInstance().getKBCreateVmInsertApp());
+				
+				//prediction is also performed therefore the classifier and the evaluator must be instantiated
+				if(!isOnlyLearning()) {
+					classifier = new MultilayerPerceptron();
+					classifier.buildClassifier(MoveVmToPm.getKnowledgeBase());
+					evaluation = new Evaluation(MoveVmToPm.getKnowledgeBase());
+					evaluation.crossValidateModel(classifier, knowledgeBase, 10, knowledgeBase.getRandomNumberGenerator(randomData.nextLong(1, 1000)));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return knowledgeBase;
+	}
+	
+	@Override
+	public void init(Resource problemVm) {
+		this.preconditionsOk = false;
+		this.selectedPm = null;
+		this.costs = 0;
+		this.vm = null;
+		
+		MoveVmToPm.vmStartupCosts = Configuration.getInstance().getVmStartupCosts();
+		
+		if (problemVm instanceof VirtualMachine){
+			this.vm = (VirtualMachine) problemVm;
+			boolean found = false;
+			int fitFactor = 0;
+			
+			for (PhysicalMachine pm : Monitor.getInstance().getPms()) {
+				if (pm.isRunning())  {
+					if ((100 - pm.getCurrentCpuAllocation()) >= vm.getCurrentCpuAllocation() && (100 - pm.getCurrentMemoryAllocation()) >= vm.getCurrentMemoryAllocation() && (100 - pm.getCurrentCpuAllocation()) >= vm.getCurrentStorageAllocation()) {
+						found = true;
+						this.costs = vmStartupCosts;
+						
+						if (this.selectedPm == null || this.calculateFit(this.vm, pm) > fitFactor) {
+							this.selectedPm = pm;
+						}
+					}
+				}
+			}
+			
+			preconditionsOk = found;
+		}
+		else {
+			preconditionsOk= false;
+		}
+	}
+	
+	/**
+	 * Calculate a fit value: How good does the VM fit into the PM?
+	 * 
+	 * @param vm VM that should be transfered
+	 * @param pm PM that should be calculated the fit value against
+	 * @return Fit value
+	 */
+	private int calculateFit(VirtualMachine vm, PhysicalMachine pm) {
+		int cpuFit = pm.getCurrentCpuAllocation() - vm.getCurrentCpuAllocation();
+		int memFit = pm.getCurrentMemoryAllocation() - vm.getCurrentMemoryAllocation();
+		int storageFit = pm.getCurrentStorageAllocation() - vm.getCurrentStorageAllocation();
+		
+		return (100 - cpuFit) + (100 - memFit) + (100 - storageFit);
+	}
+
+	@Override
+	public int predict() {
+		int output = 0;
+		
+		if (isOnlyLearning()) { // Randomized predictions for learning
+			return randomData.nextInt(0, 100);
+		} else { // Use WEKA - evaluation for prediction
+			// Create new WEKA - instance
+			Instance instance = new Instance(61);
+
+			// PM history
+			LinkedList<Integer> pmCpuHistory = this.selectedPm.getCpuAllocationHistory(10);
+			LinkedList<Integer> pmMemoryHistory = this.selectedPm.getMemoryAllocationHistory(10);
+			LinkedList<Integer> pmStorageHistory = this.selectedPm.getStorageAllocationHistory(10);
+			
+			// VM history
+			List<Integer> vmCpuHistory = this.vm.getCpuAllocationHistory(10);
+			List<Integer> vmMemoryHistory = this.vm.getMemoryAllocationHistory(10);
+			List<Integer> vmStorageHistory = this.vm.getStorageAllocationHistory(10);
+			
+			// CPU/Memory/Storage - Allocation history before the VM is moved
+			for (int i = 0; i < 10; i++) {
+				// VM history
+				instance.setValue(MoveVmToPm.getKnowledgeBase().attribute(i), clusterValue(vmCpuHistory.get(i)));
+				instance.setValue(MoveVmToPm.getKnowledgeBase().attribute(i + 10), clusterValue(vmMemoryHistory.get(i)));
+				instance.setValue(MoveVmToPm.getKnowledgeBase().attribute(i + 20), clusterValue(vmStorageHistory.get(i)));
+				
+				// PM history
+				instance.setValue(getKnowledgeBase().attribute(i + 30), clusterValue(pmCpuHistory.get(i)));
+				instance.setValue(getKnowledgeBase().attribute(i + 40), clusterValue(pmMemoryHistory.get(i)));
+				instance.setValue(getKnowledgeBase().attribute(i + 50), clusterValue(pmStorageHistory.get(i)));
+			}
+			
+			//Evaluation
+			instance.setValue(getKnowledgeBase().attribute(60), Instance.missingValue());
+	
+			instance.setDataset(CreateVmInsertApp.getKnowledgeBase());
+			
+			try {
+				output = (int) (evaluation.evaluateModelOnce(classifier, instance));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return output;
+	}
+
+	private static int clusterValue(int value) {
+		return (int) (Math.ceil(value / 10) * 10);
+	}
+	
+	@Override
+	public int estimate() {
+		return this.costs;
+	}
+
+	@Override
+	public boolean preconditions() {
+		return this.preconditionsOk;
+	}
+
+	@Override
+	public void execute() {
+		VirtualMachine vm = this.vm;
+		// remove VM from old PM
+		this.vm.getPm().getVms().remove(this.vm);
+		
+		// insert VM into new PM
+		if (this.preconditions()) {
+			this.selectedPm.getVms().add(vm);
+		}
+	}
+
+	@Override
+	public boolean evaluate() {
+		if (this.vm.getPm().getSuspendedTicks() > 0 || this.vm.getSuspendedTicks() > 0) {
+			return false;
+		} else if (waitForEvaluation > 0) {
+			waitForEvaluation--;
+			
+			return false;
+		} else {
+			// PM history
+			List<Integer> pmCpuHistory = this.selectedPm.getCpuAllocationHistory(10);
+			List<Integer> pmMemoryHistory = this.selectedPm.getMemoryAllocationHistory(10);
+			List<Integer> pmStorageHistory = this.selectedPm.getStorageAllocationHistory(10);
+			
+			// VM history
+			List<Integer> vmCpuHistory = this.vm.getCpuAllocationHistory(10);
+			List<Integer> vmMemoryHistory = this.vm.getMemoryAllocationHistory(10);
+			List<Integer> vmStorageHistory = this.vm.getStorageAllocationHistory(10);	
+			
+			//evaluate usage 
+			//(510-(abs(85-pmcpu)-abs(85-pmmem)-abs(85-pmstor)-abs(85-vmcpu)-abs(85-vmmem)-abs(85-vmstor)))/510
+			double evaluation = (510 - calculateUsageRatio(pmCpuHistory, 85) - calculateUsageRatio(pmMemoryHistory, 85) - calculateUsageRatio(pmStorageHistory, 85) - calculateUsageRatio(vmCpuHistory, 85) - calculateUsageRatio(vmMemoryHistory, 85) - calculateUsageRatio(vmStorageHistory, 85)) / 510 ;
+			
+			//subtract SLA Violations
+			List<App> apps = this.vm.getApps();
+			for (App app : apps) {
+				evaluation -= (app.getCpuSlaErrorcount()+app.getMemorySlaErrorcount()+app.getStorageSlaErrorcount())/10;
+			}
+			
+			//minimum of 0
+			evaluation = Math.max(0, evaluation);
+			
+			//Create new WEKA - instance
+			Instance inst = new Instance(34);
+			
+			//CPU/Memory/Storage - Allocation history before the new vm was created
+			//int valuesStartAt = 10-beforeInsertionCount; //if machine doesn't have 10 values before insertion
+			LinkedList<Integer> cpuallhist = selectedPm.getCpuAllocationHistory(40);
+			LinkedList<Integer> memallhist = selectedPm.getMemoryAllocationHistory(40);
+			LinkedList<Integer> storageallhist = selectedPm.getStorageAllocationHistory(40);
+			
+			for (int i = 0; i<10;i++) {
+				inst.setValue(getKnowledgeBase().attribute(i), clusterValue(cpuallhist.get(i)));
+				inst.setValue(getKnowledgeBase().attribute(i+10), clusterValue( memallhist.get(i)));
+				inst.setValue(getKnowledgeBase().attribute(i+20), clusterValue(storageallhist.get(i)));
+			}
+			
+			//Evaluation
+			inst.setValue(getKnowledgeBase().attribute(33), evaluation);
+
+			getKnowledgeBase().add(inst);
+		}
+		return true;
+	}
+
+	private static double calculateUsageRatio(List<Integer> usage, int goal) {
+		double result = 0;
+		for(int i=0; i<usage.size();i++){ 
+			result += Math.abs(85-usage.get(i))/usage.size();
+		}
+		return result;
+	}
+	
+	@Override
+	public void terminate() {
+		try {
+			Action.saveKnowledge(Configuration.getInstance().getKBCreateVmInsertApp(), MoveVmToPm.getKnowledgeBase());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
